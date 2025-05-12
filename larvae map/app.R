86c6fabb61d250bd9d6e72725f18441dda9d99d4
marine-ecologist <george.roff@csiro.au>
2025-05-12 22:46:09 +1000

    flexdashboard edits

--- larvae map/app.R
+++ larvae map/app.R
@@ -1,373 +1,125 @@
-
-if (!requireNamespace("DT", quietly = TRUE)) {
-  install.packages("DT")
-}
-library(DT)
-library(tidyverse)
-library(readxl)
-library(janitor)
-library(tidyverse)
-library(readxl)
-library(janitor)
-
-## rearing
-
-
-culture_pools_2020 <-
-  rbind(
-    # read_csv("data/2020/2.1.Witstari_Jetty_Pools_Culture_2020.csv", show_col_types = FALSE) |>
-    # clean_names() |>
-    # mutate(volume = pool_bin_volume/1000, # note this must be ml not l
-    #        density = larvae_ml*1000,
-    #        id=pool,
-    #        date = as.POSIXct(date, format = "%d/%m/%y")) |>
-    # mutate(spawndate = as.POSIXct("5/12/20", format = "%d/%m/%y")) |> # assume 5th Dec?
-    # mutate(days = as.numeric(as.POSIXct(date) - spawndate)) |>
-    # select(date, days, id, replicate, density, volume) |>
-    # mutate(id=as.factor(id),
-    #        replicate=as.factor(replicate)
-    #        ) |>
-    # mutate(year = 2020, density=as.numeric(density)),
-
-    read_csv("data/2020/2.2.OTI_Pools_Culture_2020.csv", show_col_types = FALSE) |>
-      clean_names() |>
-      drop_na() |>
-      mutate(
-        date =  as.POSIXct(date, format = "%d/%m/%y"),
-        volume = pool_bin_volume_ml/1000,
-        density = larvae_ml * 1000,
-        id = pool,
-        spawndate = case_when(
-          pool == 2 ~ as.POSIXct("05/12/20", format = "%d/%m/%y"),
-          pool == 4 ~ as.POSIXct("04/12/20", format = "%d/%m/%y"),
-          TRUE ~ NA_POSIXct_
-        )
-      ) |>
-      mutate(days = as.numeric(date - spawndate)/(60*60*24)) |>
-      select(date, days, id, replicate, density, volume) |>
-      mutate(id=as.factor(id),
-             replicate=as.factor(replicate)
-      ) |>
-      mutate(year = 2020, density=as.numeric(density))
-  ) |>
-  mutate(total = density * volume) |>
-  filter(density > 1000) |>
-  filter(volume < 100)
-
-
-
-
-culture_pools_2021 <- read_excel("data/2021/RearingLarvaeData_2021.xlsx", sheet="LarvaeDensity_Rearing") |>
-  clean_names() |>
-  mutate(days =  as.numeric(sampling_date-spawning_date),
-         date=as.POSIXct(sampling_date, format = "%d/%m/%Y")) |>
-  rename(id = pool_id) |>
-  rename(density = larvae_number_per_l) |>
-  rename(volume = pool_volume_liter) |>
-  select(date, days, id, replicate, density, volume) |>
-  mutate(across(where(is.character), ~na_if(., "NA"))) |>
-  na.omit() |>
-  mutate(id=as.factor(id),
-         replicate=as.factor(replicate)
-  ) |>
-  mutate(year = 2021, density=as.numeric(density)) |>
-  mutate(total = density * volume)
-
-
-culture_pools_2022 <- read_excel("data/2022/Larval Density Database Lizard Is 2022.xlsx", sheet = "Culturing") |>
-  janitor::clean_names() |>
-  rename(
-    volume = pool_volume,
-    replicate = sample_replicate,
-    days=time,
-    id=pool_no
-  ) |>
-  mutate(
-    density = suppressWarnings(as.numeric(larvae_per_ml)) *1000
-  ) |>
-  select(date, days, id, replicate, density, volume) |>
-  na.omit() |>
-  mutate(id=as.factor(id),
-         replicate=as.factor(replicate),
-         date=as.POSIXct(date,format = "%d/%m/%Y")
-  ) |>
-  mutate(year = 2022, density=as.numeric(density)) |>
-  mutate(total = density * volume)
-
-
-culture_pools_2023 <- read_excel("data/2023/DATA_MC_Collection_Cultures.xlsx", sheet="data_CultureClean") |>
-  clean_names() |>
-  mutate(date = date_sample,
-         days =  as.numeric(date_sample-date_collection)/(60*60*24)) |>
-  rename(volume = culture_volume_l) |>
-  rename(sample_volume = sample_volume_m_l) |>
-  rename(id = pool_tank_number) |>
-  rename(replicate =  sample_number) |>
-  rename(density = sample_volume) |>
-  mutate(across(where(is.character), ~na_if(., "NA"))) |>
-  mutate(density = as.numeric(density)) |>
-  select(date, days, id, replicate, density, volume) |>
-  mutate(id=as.factor(id),
-         replicate=as.factor(replicate)
-  ) |>
-  mutate(year = 2023, density=as.numeric(density), volume=as.numeric(volume)) |>
-  mutate(total = density * volume)
-
-
-
-culture_pools_2024 <- read_excel("data/2024/DATA_MC_SpawningOps_DataSheets_Nov-Dec2024.xlsx", sheet="data_LarvCulture1-6_CLEAN") |>
-  clean_names() |>
-  filter(volume_culture_l == 9280.0) |>
-  mutate(
-    sampling_date = suppressWarnings(
-      if_else(
-        grepl("^\\d{1,2}/\\d{1,2}/\\d{4}$", sampling_date),
-        as.Date(sampling_date, format = "%d/%m/%Y"),
-        as.Date(as.numeric(sampling_date), origin = "1899-12-30")
-      )
-    )
-  ) |>
-  mutate(
-    sampling_date = as.Date(sampling_date, format = "%d/%m/%Y"),
-    days = as.numeric(sampling_date - as.Date(spawning_date)),
-    replicate = pool_id,
-    id = sample_id,
-  ) |>
-  mutate(density = larvae_count_perml * 1000) |>
-  mutate(volume = volume_culture_l,
-         date = sampling_date) |>
-  select(date, days, id, replicate, density, volume) |>
-  mutate(id=as.factor(id),
-         replicate=as.factor(replicate)
-  ) |>
-  mutate(year = 2024, density=as.numeric(density), days = as.factor(days), total=volume*density)
-
-remove_outliers_se <- function(df, se_mult = 2) {
-  se <- sd(df$density, na.rm = TRUE) / sqrt(sum(!is.na(df$density)))
-  m <- mean(df$density, na.rm = TRUE)
-  dplyr::filter(df, abs(density - m) <= se_mult * se)
-}
-
-
-culture_pools <- rbind(culture_pools_2020,
-                       culture_pools_2021,
-                       culture_pools_2022,
-                       culture_pools_2023,
-                       culture_pools_2024) |>
-  mutate(days=as.factor(days),
-         year=as.factor(year),
-         volume = as.numeric(volume)) |>
-  drop_na(density)
-
-# pool-mean
-culture_pools_mean_id <- culture_pools |>
-  group_by(year, days, id) |>
-  summarise(density = mean(density), .groups = "drop")
-
-# day-mean
-culture_pools_mean <- culture_pools_mean_id |>
-  group_by(year, days) |>
-  summarise(
-    density_mean = round(mean(density),0),
-    n = n(),
-    sd = round(sd(density),1),
-    se = round(sd(density) / sqrt(n()),1),
-    ci95 = round(qt(0.975, df = n() - 1) * sd(density) / sqrt(n()),1),
-    .groups = "drop"
-  ) |> rename(density = density_mean)
-
-culture_pools_sum <- culture_pools |>
-  group_by(year, days) |>
-  summarise(
-    total = round(sum(density),0),
-    sd = round(sd(density),1),
-    se = round(sd / sqrt(n()),1),
-    ci95 = round(qt(0.975, df = n() - 1) * se,1),
-    .groups = "drop"
-  )
-
-
-
+# app.R
 
 library(shiny)
-library(tidyverse)
-library(scales)
-library(ggbeeswarm)
 library(shinyWidgets)
+library(tidyverse)
+library(sf)
+library(tmap)
+
+# DMS to DD conversion
+
+dms_to_dd <- function(dms) {
+  dms <- gsub("°", " ", dms)
+  dms <- gsub("'", "", dms)
+  parts <- strsplit(dms, " ")[[1]]
+  deg <- as.numeric(parts[2])
+  min <- as.numeric(parts[3])
+  dd <- deg + min / 60
+  if (grepl("S", parts[1]) | grepl("W", parts[1])) dd <- -dd
+  return(dd)
+}
+
+# Data setup
+
+pool_2020 <- data.frame(
+  id = c(
+    "Pipe1_release_001", "Pipe2_release_002",
+    "ASV1_release_003", "ASV2_release_004",
+    "Control1_005", "Control2_006"
+  ),
+  lon = c(151.9044, 151.9080, 151.9084, 151.9082, 151.9093, 151.9071),
+  lat = c(-23.45744, -23.45849, -23.45953, -23.45889, -23.45998, -23.45878)
+) |> mutate(year = 2020) |> st_as_sf(coords = c("lon", "lat"), crs = 4326)
+
+pool_2021 <- data.frame(
+  id = c(7, 8, 9, 10, 11),
+  lat = c(-14.69380, -14.69435, -14.69158, -14.69125, -14.69350),
+  lon = c(145.46037, 145.46031, 145.46047, 145.46077, 145.46030)
+) |> mutate(year=2021) |> st_as_sf(coords = c("lon", "lat"), crs = 4326)
+
+pool_2022 <- data.frame(
+  id = c(4, 6, 7, 8, "SP"),
+  lat = c(-14.693783, -14.693233, -14.693233, -14.692867, -14.698499),
+  lon = c(145.458017, 145.458033, 145.458167, 145.458183, 145.444637)
+) |> mutate(year=2022) |> st_as_sf(coords = c("lon", "lat"), crs = 4326)
+
+pool_2023 <- data.frame(
+  id = c(
+    "Liz_CoolPoolCatcher",
+    "Lizard_Pool_4",
+    "Lizard_Pool_5",
+    "Eyrie_SettPool2",
+    "Eyrie_SettPool4"
+  ),
+  lat = c(-14.697801, -14.693853, -14.693588, -14.719, -14.7193),
+  lon = c(145.444103, 145.457626, 145.45725, 145.3639, 145.3633)
+) |> mutate(year=2023) |> st_as_sf(coords = c("lon", "lat"), crs = 4326)
+
+pool_2024 <- data.frame(
+  id = 1:6,
+  Lat_dms = c("S 14°39.0986'", "S 14°39.0848'", "S 14°39.0529'", "S 14°39.0592'", "S 14°38.8209'", "S 14°38.8318'"),
+  Lon_dms = c("E 145°26.9884'", "E 145°26.9832'", "E 145°26.9836'", "E 145°27.0004'", "E 145°27.1678'", "E 145°27.1764'")
+) |> mutate(lat = sapply(Lat_dms, dms_to_dd)) |> mutate(lon = sapply(Lon_dms, dms_to_dd)) |> select(-Lon_dms, -Lat_dms) |> mutate(year=2024) |> st_as_sf(coords = c("lon", "lat"), crs = 4326)
+
+pool_sf <- rbind(pool_2020, pool_2021, pool_2022, pool_2023, pool_2024)
 
 ui <- fluidPage(
-  titlePanel("Larval Culture Summary"),
-
-  # Row 1: Sidebar and Plot
-  fluidRow(
-    column(
-      width = 3,
-      wellPanel(
-        radioGroupButtons(
-          inputId = "geom",
-          label = "Plot Type:",
-          choices = c("Bar plots" = "col", "Violin plots" = "violin"),
-          selected = "col",
-          justified = TRUE,
-          status = "primary"
-        ),
-        checkboxGroupButtons(
-          inputId = "year_filter",
-          label = "Filter Year(s):",
-          choices = levels(culture_pools$year),
-          selected = levels(culture_pools$year)[2:5],
-          justified = FALSE,
-          status = "primary",
-          checkIcon = list(yes = icon("check-square"), no = icon("square"))
-        )
-      ),
-      wellPanel(
-        selectInput("yvar", "Y-axis Variable:",
-                    choices = c(
-                      "Total larval production" = "total",
-                      "Mean larval production per pool" = "mean_pool",
-                      "Mean larval production per litre" = "per_litre"
-                    )),
-        HTML("<b>Free Y-axis</b><br><br>"),
-        switchInput(
-          inputId = "freeScale",
-          label = NULL,
-          size = "mini",
-          #status = "primary",
-          value = FALSE,
-          #inline = FALSE
-        ),
-        fluidRow(
-          # column(
-          #   width = 6,
-          #   awesomeRadio(
-          #     inputId = "facet_nrow",
-          #     label = "Facet Rows:",
-          #     choices = 1:4,
-          #     selected = 2,
-          #     inline = TRUE,
-          #     status = "primary"
-          #   )
-          # ),
-          column(
-            width = 6,
-            awesomeRadio(
-              inputId = "facet_ncol",
-              label = "Number of Columns:",
-              choices = 1:4,
-              selected = 3,
-              inline = TRUE,
-              status = "primary"
-            )
+  theme = bslib::bs_theme(version = 4, bootswatch = "cosmo"),
+  tags$head(
+    tags$style(HTML('
+      html, body {height: 100%; margin: 0; overflow: hidden;}
+      .page-container {display: flex; flex-direction: row; height: 100vh; border-radius: 12px;}
+      .sidebar-fixed {width: 300px; min-width: 250px; background-color: #E3E9F2; padding: 15px; overflow-y: auto; border-right: 1px solid #ccc;}
+      .main-scroll {flex-grow: 1; padding: 20px; overflow-y: auto; background-color: #f8f9fa;}
+      .titlebar {background-color: #427EDC; color: white; padding: 20px; font-size: 24px;}
+      .well {border-radius: 12px; padding: 15px; border: 1px solid #ccc; background-color: #DCE2EA; margin-bottom: 10px;}
+      .wellmain {border-radius: 12px; padding: 15px; background-color: #ffffff; border: 1px solid #ccc; margin-bottom: 20px;}
+    '))
+  ),
+  div(style = "max-width: 1200px; margin: 0 auto;",
+      div(class = "titlebar", "MC Larval Culturing Location Maps"),
+      div(class = "page-container",
+          div(class = "sidebar-fixed",
+              div(class = "well",
+                  selectInput(
+                    "year_select",
+                    "Select Year:",
+                    choices = setNames(c(2020, 2021, 2022, 2023, 2024), c("2020 - Wistari Reef", "2021 - Lizard Island", "2022 - Lizard Island", "2023 - Lizard Island / Eyrie Reef", "2024 - Lizard Island")),
+                    selected = 2024
+                  )
+              )
+          ),
+          div(class = "main-scroll",
+              div(class = "wellmain",
+                  tmapOutput("map_single", height = "700px")
+              )
           )
-        ),
-        # numericInput("facet_nrow", "Facet Rows:", value = 2, min = 1),
-        # numericInput("facet_ncol", "Facet Columns:", value = 3, min = 1)
-      ),
-      wellPanel(
-        sliderInput("plotHeight", "Plot Height (px):", min = 400, max = 1000, value = 600, step = 100),
-        sliderInput("plotWidth", "Plot Width (px):", min = 600, max = 1600, value = 1200, step = 100)
       )
-    ),
-    column(
-      width = 9,
-        wellPanel(
-         style = "background-color: white;",
-         div(style = "width: 80%; margin: 0 auto;",
-           uiOutput("plot_ui"),
-         )
-        ),
-      wellPanel(
-        style = "background-color: white;",
-        div(style = "width: 80%; margin: 0 auto;",
-          dataTableOutput("table")
-        )
-      )
-    )
   )
 )
 
 server <- function(input, output, session) {
+  tmap_mode("view")
 
-  output$plot_ui <- renderUI({
-    plotOutput(
-      outputId = "plot",
-      height = paste0(input$plotHeight, "px"),
-      width = paste0(input$plotWidth, "px")
-    )
+  output$map_single <- renderTmap({
+    req(input$year_select)
+    pool_sf_filtered <- pool_sf %>% filter(year == input$year_select)
+
+    tm_shape(pool_sf_filtered) +
+      tm_symbols(
+        fill = "year",
+        fill.scale = tm_scale_categorical("brewer_spectral"),
+        shape = 21,
+        size = 1.5,
+        fill.free = TRUE,
+        fill_alpha = 0.8,
+        fill.legend = tm_legend_hide()
+      ) +
+      tm_labels("id", col = "white", xmod = 1.5, ymod = 0.5) +
+      tm_basemap("Esri.WorldImagery") +
+      tm_credits("")
   })
-
-
-  output$plot <- renderPlot({
-    scale_mode <- if (input$freeScale) "free_y" else "fixed"
-
-    filtered_mean <- culture_pools_mean %>% filter(year %in% input$year_filter)
-    filtered_mean_id <- culture_pools_mean_id %>% filter(year %in% input$year_filter)
-    filtered_sum <- culture_pools_sum %>% filter(year %in% input$year_filter)
-
-    p <- if (input$geom == "col") {
-      if (input$yvar == "total") {
-        ggplot(filtered_sum, aes(x = days, y = total, fill = days)) +
-          geom_col(color = "black", linewidth = 0.5, show.legend = FALSE) +
-          geom_errorbar(aes(ymin = total - ci95, ymax = total + ci95), width = 0.3) +
-          facet_wrap(~year, scales = scale_mode,  ncol = as.numeric(input$facet_ncol)) +
-          scale_fill_brewer(palette = "YlGnBu", direction = 1) +
-          ylab("Total larval production")
-      } else if (input$yvar == "mean_pool") {
-        ggplot(filtered_mean, aes(x = days, y = density, fill = days)) +
-          geom_col(color = "black", linewidth = 0.5, show.legend = FALSE) +
-          geom_errorbar(aes(ymin = density - ci95, ymax = density + ci95), width = 0.3) +
-          facet_wrap(~year, scales = scale_mode,  ncol = as.numeric(input$facet_ncol)) +
-          scale_fill_brewer(palette = "YlGnBu", direction = 1) +
-          ylab("Mean larval production per pool")
-      } else {
-        ggplot(filtered_mean_id, aes(x = days, y = density, fill = days)) +
-          stat_summary(geom = "col", fun = mean, linewidth = 0.5, color = "black", show.legend = FALSE) +
-          stat_summary(geom = "errorbar", fun.data = mean_cl_normal, width = 0.3, show.legend = FALSE) +
-          facet_wrap(~year, scales = scale_mode,  ncol = as.numeric(input$facet_ncol)) +
-          scale_fill_brewer(palette = "YlGnBu", direction = 1) +
-          ylab("Mean larval production per litre")
-      }
-    } else {
-      df <- switch(input$yvar,
-                   "total" = filtered_sum |> mutate(density = total),
-                   "mean_pool" = filtered_mean,
-                   "per_litre" = filtered_mean_id
-      )
-      label <- switch(input$yvar,
-                      "per_litre" = "Mean larval production per litre",
-                      "mean_pool" = "Mean larval production per pool",
-                      "total" = "Total larval production"
-      )
-
-      ggplot(df, aes(x = days, y = density, fill = days)) +
-        geom_violin(trim = FALSE, color = "black", linewidth = 0.5, show.legend = FALSE) +
-        geom_beeswarm(size = 1.5, alpha = 0.8, show.legend = FALSE) +
-        facet_wrap(~year, scales = scale_mode,  ncol = as.numeric(input$facet_ncol)) +
-        scale_fill_brewer(palette = "YlGnBu", direction = 1) +
-        ylab(label)
-    }
-
-    p + theme_bw(base_size = 16)
-  },
-  height = function() input$plotHeight,
-  width = function() input$plotWidth
-  )
-
-  output$table <- renderUI({
-    df <- if (input$yvar == "total") {
-      culture_pools_sum %>% filter(year %in% input$year_filter)
-    } else if (input$yvar == "mean_pool") {
-      culture_pools_mean %>% filter(year %in% input$year_filter)
-    } else {
-      culture_pools_mean_id %>% filter(year %in% input$year_filter)
-    }
-
-    df %>%
-      knitr::kable(format = "html", digits = 2) %>%
-      kableExtra::kable_styling("striped", full_width = TRUE) %>%
-      HTML()
-  })
+}
 
 
 shinyApp(ui, server)
